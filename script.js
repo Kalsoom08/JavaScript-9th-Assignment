7dca860d93faa7bf3d29cb1048e489133f9130ec
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-07-07 11:32:50 +0500

    Class-Task

---
+++ script.js
@@ -0,0 +1,71 @@
+//Table Of A Number
+/*var tableNumber = +prompt("Please enter a number");
+for (var i = 1; i <= 10; i++) {
+   console.log(`${tableNumber} * ${i} = ${tableNumber * i}`);
+}*/
+
+
+//Sum Of Numbers
+
+/* var arr = [];
+ var sum = 0;
+ for(var i = 0 ; i < 5; i++){
+     var enterNumber = +prompt("Enter Number");
+     arr.push(enterNumber); 
+ }
+ for (let i = 0; i < 5; i++) {
+     sum = sum + arr[i]; 
+     //sum += arr[i];
+ }
+ console.log(sum);*/
+
+
+ //Largest Number
+ /*
+    var arr = [];
+    for(var i = 0; i < 5; i++){
+        var enterNumber = +prompt("Enter Number");
+        arr.push(enterNumber); 
+    }
+    var largest = arr[0];
+    for(var i = 0; i < 5; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    console.log(largest)    
+    */
+
+
+// Reverse
+ 
+/*var stringKaName = prompt("Enter A String")
+//Empty qk hum ne es me reverse value catch karni hai 
+ //stringKaReverse = ""; 
+for(var i = stringKaName.length; i >= 0; i--){
+    //hum yaha direct bi declare kr sakthe hai lekn ek character underfined hoga
+    //qk hr dafa pehle empty string create hoga
+ var stringKaReverse = stringKaReverse + stringKaName[i] 
+}
+console.log(`The value of ${stringKaName} After Reverse is ${stringKaReverse}`)
+*/
+
+
+ //Factorial
+/*var number = +prompt("Enter Number")
+if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
+else {
+    var factorialNumb = 1;
+    for (i = 1; i <= number; i++) {
+        factorialNumb =  factorialNumb * i;
+    }
+    console.log(factorialNumb);
+}*/
+/*i = 1: / 1 * 1 = 1
+i = 2: / 1 * 2 = 2
+i = 3: / 2 * 3 = 6
+i = 4: / 6 * 4 = 24
+i = 5: / 24 * 5 = 120*/
+
